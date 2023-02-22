3aa4fea05da6f6d0c27146ff1433f4716883c47a
SRAVYA64 <85957108+SRAVYA64@users.noreply.github.com>
2023-02-22 15:44:36 +0530

    Add files via upload

---
+++ strexp1.rb
@@ -0,0 +1,22 @@
+#print string n times
+begin
+class Str
+  def string
+    n = gets
+    #puts n
+    ip = gets.chomp.to_i
+    for a in 1..ip do
+      puts n
+    end 
+  end
+end
+obj = Str.new
+obj.string
+end
+class Str
+  def string(str,n)
+    return str*n
+  end
+end
+obj = Str.new
+obj.string('name1',2)
